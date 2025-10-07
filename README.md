4af6bbb867269ada4efb279e92ce9077b7c1ee91
fulltavies-wq <fulltavies@gmail.com>
2025-10-07 17:27:38 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-new-s9ccspi3
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/fulltavies-wq/angular-new-s9ccspi3)
---
+++ src/app/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <h1>ДЗ выполнено! 🎉</h1>
+    <p>app.component.html очищен, остался только router-outlet</p>
+  `,
+})
+export class HomeComponent {}
